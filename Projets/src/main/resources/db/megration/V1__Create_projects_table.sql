a00322ba3a187e2682d064571a50919da8569541
hicham-oualla <hich2022am.oualla@gmail.com>
2024-09-01 23:39:42 +0100

    v1 PROJECT table flyway

---
+++ Projets/src/main/resources/db/megration/V1__Create_projects_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projets (
+                         id SERIAL PRIMARY KEY,
+                         nom VARCHAR(255) ,
+                         description VARCHAR(255),
+                         dateDebut DATE,
+                         dateFin DATE,
+                         budget DOUBLE
